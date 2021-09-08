75a41cfc192c2c08bc3416909d47c8bd19c87c64
unknown <mohamedhanafihassanet@gmail.com>
2021-09-08 12:14:43 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild,ElementRef } from '@angular/core';
+import * as htmlToImage from 'html-to-image';
+
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-root',
@@ -9,23 +12,51 @@ export class AppComponent {
 
   title = 'test-fact';
   num=0;
-  randomArray:number[]=[0];
+  randomArray:number[]=[];
   randomArrayColors:number[][]=[];
+  // @ViewChild('screen') screen: ElementRef<HTMLInputElement> = {} as ElementRef
+  // @ViewChild('canvas') canvas: ElementRef<HTMLInputElement> = {} as ElementRef;
+  // @ViewChild('downloadLink') downloadLink: ElementRef<HTMLInputElement> = {} as ElementRef;
+  // downloadImage(){
+  //   html2canvas(this.screen.nativeElement).then(canvas => {
+  //     this.canvas.nativeElement.src = canvas.toDataURL();
+  //     this.downloadLink.nativeElement.href = canvas.toDataURL('image/png');
+  //     // this.downloadLink.nativeElement.download = 'marble-diagram.png';
+  //     this.downloadLink.nativeElement.click();
+  //   });
+  // }
   generateNumbers(){
-    console.log("1")
     // this.randomArray=[]
     for (let i = 1; i <= this.num; i++) {
       this.randomArray.push(Math.floor(Math.random() * (100 + 100 + 1)) - 100);
     }
-    console.log(typeof(this.randomArray))
-    console.log(typeof(this.randomArrayColors))
     // this.randomArrayColors=this.randomArray.map(el=> el<0? el/100:  el*10 )
-    this.randomArrayColors =this.randomArray.map((el, i) =>  el <= 0
-            // 100+el
-              ? [Math.round(Math.abs((255 * (100+el)) / 100)), Math.round(Math.abs((255 * el) / 100)), 0]
-              : [Math.round(Math.abs((255 * el) / 100)), Math.round(Math.abs((255 * (100-el)) / 100)), 0]
-          )
-
+    this.randomArrayColors =this.randomArray.map((el, i) =>
+    el === 0
+    // g (el*255)/200
+      ? [127.5,
+         127.5, 0]
+        //  r
+      :  el<0? [Math.round(((100-Math.abs(el))/200)*255),
+         Math.round(((Math.abs(el)+100)/200)*255), 0] 
+         : 
+         [Math.round(((Math.abs(el)+100)/200)*255),
+          Math.round(((100-Math.abs(el))/200)*255), 0]
+  )
   }
+  
+  generateImage(){
+    var node:any = document.getElementById('image-section');
+    htmlToImage.toPng(node)
+      .then(function (dataUrl) {
+        var img = new Image();
+        img.src = dataUrl;
+        document.body.appendChild(img);
+      })
+      .catch(function (error) {
+        console.error('oops, something went wrong!', error);
+      });
+  }
+
  
 }
